80e2b90faa3957e74a478895701e275eb99a1c12
jrbartowski <joaorgpires@gmail.com>
2015-04-01 15:17:55 +0100

    Add minimax.cpp

---
+++ minimax.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define W 1
+#define D 0
+#define L -1
+
+using namespace std;
+
+struct Node {
+  char state[3][3];
+};
+
+void utility() {
+  
+}
+
+void terminal_test(Node cur) {
+  
+}
+
+void minimax_decision(Node cur) {
+  
+}
+
+void max_value(Node cur) {
+  
+}
+
+void min_value(Node cur) {
+  
+}
+
+char xoro(int pl) {
+  if(pl == 1)
+    return 'X';
+  
+  else
+    return 'O';
+}
+
+int main() {
+  printf("Player 1 or 2? ");
+  int pl; scanf("%d", &pl); //player1 or player2
+  
+  
+  
+  return 0;
+}
